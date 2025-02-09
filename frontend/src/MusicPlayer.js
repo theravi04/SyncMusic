71f289dbdefcd15cb0c52d42b6d6fd9f554c9bfe
Ravi Kumar <04ravi.rk@gmail.com>
2025-02-09 19:54:06 +0530

    final

--- frontend/src/MusicPlayer.js
+++ frontend/src/MusicPlayer.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://syncmusic-z92n.onrender.com/");
 
 export default function MusicPlayer() {
   const audioRef = useRef(null);
